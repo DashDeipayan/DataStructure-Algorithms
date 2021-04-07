c098b25106dd66ddb350ace93d46058a3ac42e11
Deipayan Dash <deipayan.dash8@gmail.com>
2021-04-07 05:38:06 +0530

    Added helpful comments😛

--- Binary Search Tree/BinarySearchTree.java
+++ Binary Search Tree/BinarySearchTree.java
@@ -2,7 +2,7 @@ public class BinarySearchTree {
     class Node {
         int value;
         Node left, right;
-
+		//Making a class for node
         public Node(int value) {
             this.value = value;
             left = right = null;
@@ -10,16 +10,18 @@ public class BinarySearchTree {
     }
     Node root;
 
+	//Constructor
     BinarySearchTree() {
         root = null;
     }
 
+	//Helper function for inserting node
     public Node insertNode(Node root, int value) {
-        if (root == null) {
-            root = new Node(value);
+        if (root == null) {			
+            root = new Node(value);			//set up root if empty prior
             return root;
         }
-        if(value<=root.value){
+        if(value<=root.value){				//Following property of BST
             root.left = insertNode(root.left, value);
         }
         else{
@@ -28,6 +30,7 @@ public class BinarySearchTree {
         return root;
     }
 
+	//Helper function for node contains
     public Boolean containsNode(Node root, int value) {
         if (root == null) {
             return false;
@@ -45,6 +48,7 @@ public class BinarySearchTree {
         return false;
     }
 
+	//Helper function for deleting a node and reconstructing the BST 
     public Node deleteNode(int value, Node root){
         if(root == null)
             return root;
@@ -61,12 +65,13 @@ public class BinarySearchTree {
             else if(root.right == null){
                 return root.left;
             }
-            root.value = inOrderSuccessor(root.right);
-            root.right = deleteNode(root.value, root.right);
+            root.value = inOrderSuccessor(root.right);			//Find the inorder successor and replace with
+            root.right = deleteNode(root.value, root.right);	//deleted node
         }
         return root;
     }
 
+	//functio to find inorder successor
     private int inOrderSuccessor(Node root) {
         int minimumValue = root.value;
         while(root.left != null){
@@ -76,6 +81,7 @@ public class BinarySearchTree {
         return minimumValue;
     }
 
+	//Helper function for inorder inorder traversal
     public void inOrderTraversal(Node root){
         if(root != null){
             inOrderTraversal(root.left);
@@ -84,19 +90,28 @@ public class BinarySearchTree {
         }
     }
 
+	//Main function for inserting a node into the BinarySearchTree
     void insert(int value){
         root = insertNode(root,value);
     }
+
+	//Main function for checking a node in BST
     void contains(int value){
         System.out.println("\nTree contains "+value+" = "+containsNode(root,value));  
     }
+
+	//Main function for deleting a node from the BinarySearchTree
     void delete(int value){
         System.out.println("\nDeleting node with value "+value);
         root = deleteNode(value,root);
     }
+
+	//Main function for inorder traversal of the tree
     void traverse(){
         inOrderTraversal(root);
     }
+
+	//Main function for finding the closest Value in BST
     int findClosest(int target){
         return FindClosestValue.findClosestValue(root,target);
     }
