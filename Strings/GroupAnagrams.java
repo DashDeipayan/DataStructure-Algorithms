f2730ccdfbe1371e6f11428b7955d5109fbdb043
Deipayan Dash <deipayan.dash8@gmail.com>
2021-04-11 11:54:46 +0530

    Rearranged code in grouping anagrams problems

--- Strings/GroupAnagrams.java
+++ Strings/GroupAnagrams.java
@@ -7,7 +7,19 @@ public class GroupAnagrams {
         return new String(tempArray);
     }
 
-    private static String[] groupAnagrams(String[] words) {
+    private static String[] groupAnagramsMapValues(HashMap<String, ArrayList<String>> map, int size) {
+        String[] groupAnagrams = new String[size];
+        int i = 0;
+        for (String keyName : map.keySet()) {
+            String keyString = keyName.toString();
+            String value = map.get(keyString).toString();
+            groupAnagrams[i] = value;
+            i++;
+        }
+        return groupAnagrams;
+    }
+
+    public static String[] groupAnagrams(String[] words) {
         HashMap<String, ArrayList<String>> sortedWordsMap = new HashMap<String, ArrayList<String>>();
         for (String word : words) {
             String keyWord = sortedString(word);
@@ -20,23 +32,11 @@ public class GroupAnagrams {
         return groupAnagramsMapValues(sortedWordsMap, size);
     }
 
-    public static String[] groupAnagramsMapValues(HashMap<String, ArrayList<String>> map, int size) {
-        String[] groupAnagrams = new String[size];
-        int i = 0;
-        for (String keyName : map.keySet()) {
-            String keyString = keyName.toString();
-            String value = map.get(keyString).toString();
-            groupAnagrams[i] = value;
-            i++;
-        }
-        return groupAnagrams;
-    }
-
     public static void main(String[] args) {
-        String[] set1 = { "tor", "fo", "rofl", "of", "forl", "ort", "flor" };
+        String[] set1 = { "tor", "omg", "lmfao", "rofl", "mog", "fmoal", "forl", "ort", "flor" };
         System.out.println(Arrays.toString(groupAnagrams(set1)));
 
-        String[] set2 = { "tor", "fo" };
+        String[] set2 = { "omg", "tor", "", "" };
         System.out.println(Arrays.toString(groupAnagrams(set2)));
 
         String[] set3 = { "blah" };
