3ee3fec4aae07144b88abb48d1582d393cc9ba04
Deipayan Dash <deipayan.dash8@gmail.com>
2021-04-12 11:15:55 +0530

    Fixed checkIFPalindrome error

--- Strings/CheckIfPalindrome.java
+++ Strings/CheckIfPalindrome.java
@@ -1,30 +1,30 @@
 public class CheckIfPalindrome {
-    public static boolean isPalindromeExtraTime(String string){
+    public static boolean isPalindromeExtraTime(String string) {
         String reversedString = "";
         int size = string.length();
-        for (int i=size-1; i>=0;i--){
-            reversedString += string.charAt(i);         //O(n^2): Time  ||  O(n):space
+        for (int i = size - 1; i >= 0; i--) {
+            reversedString += string.charAt(i); // O(n^2): Time || O(n):space
         }
-        return string==reversedString;
+        return string == reversedString;
     }
 
-    public static boolean isPalindrome(String string){
+    public static boolean isPalindrome(String string) {
         int leftIdx = 0;
-        int rightIdx = string.length();
-        while (leftIdx<rightIdx){
-            if(string.charAt(leftIdx)!=string.charAt(rightIdx)){
+        int rightIdx = string.length() - 1;
+        while (leftIdx < rightIdx) {
+            if (string.charAt(leftIdx) != string.charAt(rightIdx)) {
                 return false;
             }
             leftIdx++;
             rightIdx--;
         }
-        return true;                                    //O(n):Time || O(1):space
+        return true; // O(n):Time || O(1):space
     }
-    
+
     public static void main(String[] args) {
         String string1 = "abcddcba";
         String string2 = "abcdef";
-        System.out.println("String1 is isPalindrome: "+isPalindrome(string1));
-        System.out.println("String2 is isPalindrome: "+isPalindromeExtraTime(string2));
+        System.out.println("String1 is isPalindrome: " + isPalindrome(string1));
+        System.out.println("String2 is isPalindrome: " + isPalindromeExtraTime(string2));
     }
 }
