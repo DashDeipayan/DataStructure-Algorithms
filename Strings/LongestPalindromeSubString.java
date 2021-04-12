0dbf168e86572840e4a364e2b49d5228a8009aee
Deipayan Dash <deipayan.dash8@gmail.com>
2021-04-12 05:41:50 +0530

    Added Longest Substring without duplicates problem

--- Strings/LongestPalindromeSubString.java
+++ Strings/LongestPalindromeSubString.java
@@ -1,39 +1,39 @@
-public class LongestPalindromeSubString{
-    public static void printSubString(String str, int left, int right){
+public class LongestPalindromeSubString {
+    public static void printSubString(String str, int left, int right) {
         System.out.println(str.substring(left, right));
-    } 
+    }
 
-    public static void longestPalindromeSubString(String string){
-        int from = 0, to=1;
-        int left,right;
+    public static void longestPalindromeSubString(String string) {
+        int from = 0, to = 1;
+        int left, right;
         int size = string.length();
-        for (int i=1;i<size;i++){
-            left = i-1;
+        for (int i = 1; i < size; i++) {
+            left = i - 1;
             right = i;
-            while(left>=0 && right<size && string.charAt(left)==string.charAt(right)){
-                if(right-left+1>to){
+            while (left >= 0 && right < size && string.charAt(left) == string.charAt(right)) {
+                if (right - left + 1 > to) {
                     from = left;
-                    to = right-left+1;
+                    to = right - left + 1;
                 }
                 left--;
                 right++;
             }
-            left = i-1;
-            right= i+1;
-            while(left>=0 && right<size && string.charAt(left)==string.charAt(right)){
-                if(right-left+1>to){
+            left = i - 1;
+            right = i + 1;
+            while (left >= 0 && right < size && string.charAt(left) == string.charAt(right)) {
+                if (right - left + 1 > to) {
                     from = left;
-                    to = right-left+1;
+                    to = right - left + 1;
                 }
                 left--;
                 right++;
             }
         }
-        printSubString(string, from, from+to);
+        printSubString(string, from, from + to);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String str = "abcxyyzzyyxabc";
         longestPalindromeSubString(str);
-    }    
+    }
 }
