f185c705e004e7ea7881ce113b4fc6427559bb07
Deipayan Dash <deipayan.dash8@gmail.com>
2021-04-17 02:31:46 +0530

    Added test case

--- Arrays/FirstDuplicate.java
+++ Arrays/FirstDuplicate.java
@@ -13,4 +13,9 @@ public class FirstDuplicate {
         }
         return -1;
     }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8 };
+        System.out.println(firstDuplicate(array));
+    }
 }
